cdf7b2c07a21e71b74f1ccb1ef48b06540edd9ce
Alexey Medvecky <alexey.medvecky@gmail.com>
2019-04-09 10:24:54 +0000

    happy path of payments

--- payments_tcs.py
+++ payments_tcs.py
@@ -5,15 +5,17 @@ import json
 class TestPayments(object):
 
     def test_one(self):
-        url = "https://api.tieto.com/v1/payments/sepa-credit-transfers"
+        url = "https://api.tieto.com/v1/payments/instant-sepa-credit-transfers"
         headers = {
             "Content-Type": "application/json",
             "X-Request-ID": "99391c7e-ad88-49ec-a2ad-99ddcb1f7721",
-            "X-API-Key": "8ef7a502-056d-4113-adba-ac7a3c2dce0a"
+            "X-API-Key": "8ef7a502-056d-4113-adba-ac7a3c2dce0a",
+            "TPP-Redirect-URI": "https://client.example.com/code_challenge_method='S2_56'",
+            "PSU-IP-Address": "127.0.0.1"
         }
         payload = {
-            "instructedAmount": {"currency": "EUR", "amount": "123.50"},
-            "debtorAccount": {"iban": "DE40100100103307118608"},
+            "instructedAmount": {"currency": "EUR", "content": "123.50"},
+            "debtorAccount": {"iban": "BANK95273278098985433"},
             "creditorName": "Merchant123",
             "creditorAccount": {"iban": "DE02100100109307118603"},
             "remittanceInformationUnstructured": "Ref Number Merchant"
